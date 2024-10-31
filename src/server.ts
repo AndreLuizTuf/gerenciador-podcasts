5f0208341bccc7f6c706b403ee1e787b9d3c170f
André Luiz <andre.luiz.a.f@hotmail.com>
2024-10-31 10:13:13 -0300

    Criando Server Http na prática

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Olá, mundo");
+import * as http from 'http';
+
+const server = http.createServer((request: http.IncomingMessage, response:http.ServerResponse) => {})
+
+
+const port = process.env.PORT
+
+server.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`);
+})
