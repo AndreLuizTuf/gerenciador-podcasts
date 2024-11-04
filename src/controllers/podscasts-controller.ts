763019e7ec94ec36ee8ee0378f2e71708912bd02
André Luiz <andre.luiz.a.f@hotmail.com>
2024-11-04 10:29:44 -0300

    Tratamento de Erro De Status Code

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -4,6 +4,7 @@ import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
+import { FilterPodCastModel } from "../models/filter-podcast-model";
 
 export const getListEpisodes = async (
   req: IncomingMessage,
@@ -20,7 +21,7 @@ export const getFilerEpisodes = async (
   res: ServerResponse
 ) => {
   
-  const content = await serviceFilterEpisodes(req.url);
-  res.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
-  res.end(JSON.stringify(content));
+  const content: FilterPodCastModel = await serviceFilterEpisodes(req.url);
+  res.writeHead(content.statusCode, { "Content-Type": ContentType.JSON });
+  res.end(JSON.stringify(content.body));
 };
